cc527d37793d7639edc729c4b5f9db6d2f2152f5
Leila-Cl <73179482+Leila-Cl@users.noreply.github.com>
2022-12-14 14:28:04 -0500

    timer

---
+++ src/timer.js
@@ -0,0 +1,14 @@
+var timeLeft = 30;
+    var elem = document.getElementById('some_div');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }
